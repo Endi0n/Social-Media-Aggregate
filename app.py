4c0db01b2400c676f78586926f6df4d9acf2e673
octavian <coctavian10@yahoo.ro>
2020-03-25 16:20:58 +0200

    Heroku deployment testing

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
