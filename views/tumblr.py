7bd72fa1e78ee78ed025e372ee4c551aff991118
ceofil <gceofil@gmail.com>
2020-04-10 10:26:09 +0300

    tumblr get_post filter

--- views/tumblr.py
+++ views/tumblr.py
@@ -41,3 +41,57 @@ def auth_callback(user):
 @tumblr_required
 def profile(tumblr_client):
     return jsonify(tumblr_client.info())
+
+
+
+
+
+# not consistent
+
+
+def _get_post(client, post_id):
+    username = client.info()['user']['name']
+    blogname = '{}.tumblr.com'.format(username)
+    response = client.posts(blogname, id=post_id)
+    return response['posts'][0]
+
+
+
+
+def get_post(client, post_id):
+    post = _get_post(client, post_id)
+    result = dict()
+    
+    result['id'] = post['id_string']
+    result['created_at'] = post['timestamp']
+    result['note_count'] = post['note_count']
+    result['hashtags'] = post['tags']
+    result['type'] = post['type']
+
+    content = dict()
+    if post['type'] == 'text':
+        content['body'] = post['body']
+        
+    elif post['type'] == 'chat':
+        content['body'] = post['body']
+
+    elif post['type'] == 'link':
+        content['link_image'] = post['link_image']
+        content['url'] = post['url']
+        content['excerpt'] = post['excerpt']
+        content['description'] = post['description']
+
+    elif post['type'] == 'photo':
+        content['photos'] = post['photos']
+
+    elif post['type'] == 'video':
+        content['video_type'] = post['video_type']
+        content['caption'] = post['caption']
+        content['premalink_url'] = post['permalink_url']
+        content['video'] = post['video']
+
+    if 'title' in post:
+        content['title'] = post['title']
+
+    result['content'] = content
+    return result
