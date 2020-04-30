b7e19ed073ad1593c31be10b6471e34c751b487d
octavian <coctavian10@yahoo.ro>
2020-04-30 20:00:39 +0300

    Expose token for user validation
    Expose token for user validation2

--- utils/jwt.py
+++ utils/jwt.py
@@ -3,19 +3,30 @@ from datetime import datetime
 import jwt
 
 
+def encode(data):
+    data = data.copy()
+    data.update({'iat': datetime.timestamp(datetime.utcnow())})
+
+    return jwt.encode(data, app.config['SECRET_KEY']).decode()
+
+
 def generate_validate_email_token(user):
-    return jwt.encode({
-        'iat': datetime.timestamp(datetime.utcnow()),
+    return encode({
         'user_id': user.id,
         'email': user.email,
         'request_type': 'validate_email'
-    }, app.config['SECRET_KEY']).decode()
+    })
 
 
 def generate_reset_password_token(user):
-    return jwt.encode({
-        'iat': datetime.timestamp(datetime.utcnow()),
+    return encode({
         'user_id': user.id,
         'email': user.email,
         'request_type': 'reset_password'
-    }, app.config['SECRET_KEY']).decode()
+    })
+
+
+def generate_token(user):
+    return encode({
+        'user_id': user.id,
+    })
--- views/common.py
+++ views/common.py
@@ -1,9 +1,16 @@
 from flask import jsonify
 from utils.auth import verified_user_required
 from app import app
+import utils.jwt
 
 
 @app.route('/profile')
 @verified_user_required
 def profile(user):
     return jsonify(id=user.id)
+
+
+@app.route('/token')
+@verified_user_required
+def token(user):
+    return jsonify(token=utils.jwt.generate_token(user))
